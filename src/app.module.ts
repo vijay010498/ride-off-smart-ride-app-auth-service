f88c7db4a1ecc1d790c37cf3fe419d67a50ae2ae
vijay010498 <svijayr007@gmail.com>
2024-01-24 17:12:59 -0500

    add: added modules controllers and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OtpModule } from './otp/otp.module';
+import { UserModule } from './user/user.module';
+import { TokenModule } from './token/token.module';
+import { LocationModule } from './location/location.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [],
+  imports: [OtpModule, UserModule, TokenModule, LocationModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
