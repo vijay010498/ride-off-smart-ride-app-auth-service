a970b22c2744d0420e9b7f7e5151e4b9ffefcd18
vijay010498 <svijayr007@gmail.com>
2024-01-26 14:57:55 -0500

    fixed error import in profile module

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
-import { UserService } from '../user/user.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [UserService],
+  imports: [UserModule],
   controllers: [ProfileController],
   providers: [ProfileService],
 })
