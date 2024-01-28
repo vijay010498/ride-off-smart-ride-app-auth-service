95ebf60145e0d51f310d19b8435b5a029ddadcd7
vijay010498 <svijayr007@gmail.com>
2024-01-28 09:30:15 -0500

    lint changes

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,4 @@
-import {
-  Injectable,
-  Logger,
-} from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserDocument } from './user.schema';
