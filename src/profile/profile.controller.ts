87285456b7e10f6e18f63e65868680de375775b6
EnigmaTechx <derryckdxd@gmail.com>
2024-01-29 17:18:36 -0500

    completed update profile endpoint

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,9 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Patch,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ProfileService } from './profile.service';
+import { CurrentUserInterceptor } from 'src/common/interceptors/current-user.interceptor';
+import { Serialize } from 'src/common/interceptors/serialize.interceptor';
+import { UserDto } from 'src/user/dtos/user.dto';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { AccessTokenGuard } from 'src/common/guards/accessToken.guard';
+import { IsBlockedGuard } from 'src/common/guards/isBlocked.guard';
+import { TokenBlacklistGuard } from 'src/common/guards/tokenBlacklist.guard';
+import { UpdateUserDto } from 'src/user/dtos/update-user.dto';
 
 @Controller('profile')
+@UseInterceptors(CurrentUserInterceptor)
+@Serialize(UserDto)
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
-  // TODO implement Update Profile - Patch API
+  @UseGuards(AccessTokenGuard, IsBlockedGuard, TokenBlacklistGuard)
+  @Patch('/update')
+  updateProfile(@CurrentUser() user: any, @Body() body: UpdateUserDto) {
+    return this.profileService.updateProfile(user.id, body);
+  }
 }
